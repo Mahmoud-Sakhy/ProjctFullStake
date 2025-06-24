547826da0ede8f7b8f6d512b8dd332cba61c96bf
Mahmoud-Sakhy <mahmoudsakhyanwer@gmail.com>
2025-06-24 17:37:27 +0200

    first commit

--- src/app/dashboard/todos/page.tsx
+++ src/app/dashboard/todos/page.tsx
@@ -1,7 +1,12 @@
-export default function TodosPage() {
+
+import React from 'react'
+
+const TodosPage = () => {
   return (
-    <div className="p-6">
+  <div className="p-6">
       <h1 className="text-2xl font-bold">قائمة المهام</h1>
     </div>
-  );
+  )
 }
+
+export default TodosPage
